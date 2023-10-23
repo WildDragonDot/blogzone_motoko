78d753c4e8b2a4be456fcf5d6d5d9af7ebd3e2f7
WildDragonDot <chandan.mca.2019@gmail.com>
2023-10-23 22:13:36 +0530

    change in code

--- src/blogzone_backend/main.mo
+++ src/blogzone_backend/main.mo
@@ -1,5 +1,85 @@
+import Array "mo:base/Array";
+import Text "mo:base/Text";
+import Nat "mo:base/Nat";
+import Bool "mo:base/Bool";
+import Time "mo:base/Time";
+import Int64 "mo:base/Int64";
+
 actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+  type Article = {
+    id : Nat;
+    title : Text;
+    post : Text;
+    time : Int;
+  };
+
+  var posts : [Article] = [];
+  var nextId : Nat = 0;
+
+  public func add(title : Text, article : Text) : async Article {
+    let currentDate : Int = Time.now();
+    let post : Article = {
+      id = nextId;
+      post = article;
+      time = currentDate;
+      title = title;
+    };
+    nextId += 1;
+    posts := Array.append<Article>(posts, [post]); // Use Array.append to add to the array
+    return post;
+  };
+
+  public func getPosts() : async [Article] {
+    return posts;
+  };
+
+  public func singlePost(id : Nat) : async [Article] {
+    let matchingPosts : [Article] = Array.tabulate<Article>(
+      posts.size(),
+      func(index : Nat) : Article {
+        let post = posts[index];
+        if (post.id == id) {
+          return {
+            id = post.id;
+            post = post.post;
+            time = post.time;
+            title = post.title;
+          };
+        } else {
+          return post;
+        };
+      },
+    );
+
+    return matchingPosts;
+  };
+
+  public func editPost(id : Nat, article : Text, title : Text) : async () {
+    let currentDate : Int = Time.now();
+    posts := Array.tabulate<Article>(
+      posts.size(),
+      func(index : Nat) : Article {
+        let post = posts[index];
+        if (post.id == id) {
+          return {
+            id = post.id;
+            post = article;
+            time = currentDate;
+            title = title;
+          };
+        } else {
+          return post;
+        };
+      },
+    );
+  };
+
+  public func removePost(idToRemove : Nat) : async () {
+    posts := Array.filter(
+      posts,
+      func(post : Article) : Bool {
+        return post.id != idToRemove;
+      },
+    );
   };
 };
--- src/blogzone_frontend/src/index.js
+++ src/blogzone_frontend/src/index.js
@@ -1,19 +1,88 @@
 import { blogzone_backend } from "../../declarations/blogzone_backend";
 
+ClassicEditor.create(document.querySelector(".editor"), {
+  placeholder: "Type the content here!",
+  toolbar: {
+    items: [
+      "bold",
+      "italic",
+      "numberedList",
+      "|",
+      "strikethrough",
+      "specialCharacters",
+      "SelectAll",
+      "|",
+      "undo",
+      "redo",
+    ],
+  },
+  language: "en",
+  licenseKey: "",
+})
+  .then((editor) => {
+    window.editor = editor;
+  })
+  .catch((error) => {
+    console.error("Oops, something went wrong!");
+    console.error(
+      "Please, report the following error on https://github.com/ckeditor/ckeditor5/issues with the build id and the error stack trace:"
+    );
+    console.warn("Build id: pmx4yvdjalhf-y97dsflr6i51");
+    console.error(error);
+  });
+
+const totalPosts = await blogzone_backend.getPosts();
+
+const container = document.getElementById("card-container");
+function formatNanosecondsTimestamp(nanosecondsTimestamp) {
+  // Convert nanoseconds to milliseconds (1 second = 1,000,000,000 nanoseconds)
+  const millisecondsTimestamp = Number(nanosecondsTimestamp) / 1000000;
+
+  // Create a Date object using the milliseconds timestamp
+  const date = new Date(millisecondsTimestamp);
+
+  // Format the date as a human-readable string
+  const formattedDate = date.toLocaleString(); // Adjust the locale as needed
+
+  return formattedDate;
+}
+
+const color_array = ["blue", "green", "yellow", "brown", "purple", "orange"];
+
+// Use the map function to create an array of card elements
+const cardElements = totalPosts.map((postObj, key) => {
+  return `
+    <div class="col-md-6 col-sm-6 content-card" key=${key}>
+        <div class="card-big-shadow">
+            <div class="card card-just-text" data-backgrou nd="color" data-color=${
+              color_array[key]
+            } data-radius="none">
+                <div class="content">
+                    <h6 class="category">${formatNanosecondsTimestamp(
+                      postObj.time
+                    )}</h6>
+                    <h4 class="title"><a href="#">${postObj.title}</a></h4>
+                    <p class="description">${postObj.post}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  `;
+});
+
+// Join the card elements and insert them into the container
+container.innerHTML = cardElements.join("");
+
 document.querySelector("form").addEventListener("submit", async (e) => {
   e.preventDefault();
   const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
+  const editorData = editor.getData();
+  const title = document.getElementById("title").value.toString();
   button.setAttribute("disabled", true);
-
   // Interact with foo actor, calling the greet method
-  const greeting = await blogzone_backend.greet(name);
-
+  const postAdded = await blogzone_backend.add(title, editorData);
   button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
+  alert("data addeed successfully");
+  window.location.reload();
   return false;
 });
